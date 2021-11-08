8d16a04903a19828fdd47f0d72c2dc6f7d9b7f44
Valentina1502 <valan2529@gmail.com>
2021-11-08 20:24:25 +0300

    fixed

--- pr2.py
+++ pr2.py
@@ -5,8 +5,6 @@
 if __name__ == '__main__':
     tpl = lambda a, b: (a, b)
     p1 = tpl(1, 2)
-    print(f'{p1} \n ')
     p2 = tpl(p1, 3)
-    print(f'{p2} \n')
     p3 = tpl(p1, p2)
-    print(f'{p3} ')
+    print(f'{p1} \n{p2} \n{p3}')
